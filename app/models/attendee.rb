c00b5738a79417632b405270f0a70fdc45e46f48
Emma Van Rossum <emmavanrossum@Emma-Van-Rossums-MacBook-Pro.local>
2015-03-07 16:44:42 +0100

    geo

--- app/models/attendee.rb
+++ app/models/attendee.rb
@@ -1,3 +1,6 @@
 class Attendee < ActiveRecord::Base
   mount_uploader :picture, PictureUploader
+  validates :name, :address, presence: true
+  geocoded_by :address
+  after_validation :geocode
 end
