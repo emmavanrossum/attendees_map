24a7783fb1d36a0fd1107f67a395267a640c9283
Emma Van Rossum <emmavanrossum@Emma-Van-Rossums-MacBook-Pro.local>
2015-03-07 16:45:00 +0100

    root redirect

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :attendees
-  root 'attendees#index'
+  root :to => redirect('/attendees')
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
